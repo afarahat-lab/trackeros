5680421bf6fd3ffb6e3f4c2d3cd33e43786ca808
Amr Farahat <amr.farahat@gmail.com>
2026-06-19 22:14:30 +0300

    chore: reset trackeros src to clean scaffold (remove prior-run deployed phases)

--- src/modules/employee/employee.service.ts
+++
@@ -1,6 +0,0 @@
-import { Employee } from './employee.model';
-
-export interface IEmployeeService {
-  getEmployeeById(id: string): Promise<Employee | null>;
-  getManagerByEmployeeId(employeeId: string): Promise<Employee | null>;
-}
--- src/modules/notification/notification.service.ts
+++
@@ -1,5 +0,0 @@
-import { Notification, CreateNotificationDto } from './notification.model';
-
-export interface INotificationService {
-  createNotification(dto: CreateNotificationDto): Promise<Notification>;
-}
