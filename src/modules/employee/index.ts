a5e94fa14a7d3cff07b72b1a27251754d28acc71
Gestalt <gestalt@platform.local>
2026-08-18 23:51:13 +0000

    [code-agent] 4 source file(s) (corr 9e97b585)

---
+++ src/modules/employee/index.ts
@@ -0,0 +1,6 @@
+export { Employee } from './employee.model';
+export { IEmployeeRepository } from './employee.repository.interface';
+export { CreateEmployeeDto, IEmployeeService } from './employee.service.interface';
+export { EmployeeService } from './employee.service';
+export { makeEmployeeController } from './employee.controller';
+export { employeeRoutes } from './employee.routes';
