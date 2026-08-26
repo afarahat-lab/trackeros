086f042fe3ab5919c9730f67e8fbee3ff7e17c66
Gestalt <gestalt@platform.local>
2026-08-26 03:15:04 +0000

    [code-agent] 1 source file(s) (corr b4533b56)

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import Fastify from 'fastify';
 import { uptimeRoutes } from './modules/uptime/uptime.routes';
+import { leaveRoutes } from './modules/leave/leave.routes';
 
 const app = Fastify({ logger: true });
 
 app.register(uptimeRoutes);
+app.register(leaveRoutes);
 
 export default app;
